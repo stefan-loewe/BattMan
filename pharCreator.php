1ff2a822d92a4827e7a86aaf1db97e7e2a4e4d8e
Stefan Löwe <stefan@loewe.ws>
2012-08-24 06:33:42 +0000

    phar actually loaded local files, instead of files in phar - fixed

--- pharCreator.php
+++ pharCreator.php
@@ -8,17 +8,21 @@ try {
        new RecursiveDirectoryIterator('D:\\workspace\\programming\\PHP\\BM')),
       'D:\\workspace\\programming\\PHP\\BM\\');
 
+
+
 $phar->setStub('<?php
 error_reporting(E_ALL | E_STRICT);
 
-require_once \'../Woody/lib/winbinder.php\';
-require_once \'../Woody/lib/fi/freeimage.inc.php\';
-require_once \'../Woody/source/Utils/Autoload/Autoloader.inc\';
+Phar::mapPhar(\'BattMan.phar\');
 
-$woodyAutoloader = new \Utils\Autoload\Autoloader(\'../Woody/source/\');
+require_once \'phar://BattMan.phar/Woody/lib/winbinder.php\';
+require_once \'phar://BattMan.phar/Woody/lib/fi/freeimage.inc.php\';
+require_once \'phar://BattMan.phar/Woody/source/Utils/Autoload/Autoloader.inc\';
+
+$woodyAutoloader = new \Utils\Autoload\Autoloader(\'phar://BattMan.phar/Woody/source/\');
 spl_autoload_register(array($woodyAutoloader, \'autoload\'));
 
-$appAutoloader = new \Utils\Autoload\Autoloader(\'./source/\');
+$appAutoloader = new \Utils\Autoload\Autoloader(\'phar://BattMan.phar/BatteryMark/source/\');
 spl_autoload_register(array($appAutoloader, \'autoload\'));
 
 function globalWinBinderEventHandler($windowID, $id, $controlID = 0, $type = 0, $property = 0) {
