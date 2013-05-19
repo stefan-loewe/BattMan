17a0e11164fc316f967d163d97e44ed9cf14e527
Stefan Löwe <stefan@loewe.ws>
2013-05-19 18:15:31 +0200

    log file no longer hard coded but selectable when checkbox is activated; label is now shown correctly

--- source/ws/loewe/BattMan/View/LogView.php
+++ source/ws/loewe/BattMan/View/LogView.php
@@ -2,39 +2,60 @@
 
 namespace ws\loewe\BattMan\View;
 
-use \ws\loewe\BattMan\Model\Model;
-use \ws\loewe\Woody\Components\Controls\Checkbox;
-use \ws\loewe\Woody\Components\Controls\EditArea;
-use \ws\loewe\Woody\Components\Controls\Label;
-use \ws\loewe\Woody\Event\ActionAdapter;
-use \ws\loewe\Utils\Geom\Dimension;
-use \ws\loewe\Utils\Geom\Point;
+use ws\loewe\BattMan\Model\Model;
+use ws\loewe\Utils\Geom\Dimension;
+use ws\loewe\Utils\Geom\Point;
+use ws\loewe\Woody\Components\Controls\Checkbox;
+use ws\loewe\Woody\Components\Controls\EditArea;
+use ws\loewe\Woody\Components\Controls\Label;
+use ws\loewe\Woody\Dialog\FileSystem\FileSaveDialog;
+use ws\loewe\Woody\Event\ActionAdapter;
 
 class LogView extends View  {
+  /**
+   * the label of the checkbox for file logging
+   *
+   * @var Label
+   */
   private $lblDoLogToFile = null;
+  
+  /**
+   * the checkbox for file logging
+   *
+   * @var Checkbox
+   */
   private $chkDoLogToFile = null;
+  
+  /**
+   * the text area where to log is shown
+   *
+   * @var EditArea 
+   */
   private $txtLog         = null;
+
+  /**
+   * path to the file where to log to
+   *
+   * @var string
+   */
+  private $logFile        = null;
 
   public function __construct(Point $topLeftCorner, Dimension $dimension) {
     parent::__construct($topLeftCorner, $dimension);
-    
-    if(file_exists('./battman.log')) {
-      unlink('./battman.log');
-    }
   }
 
   public function initialize() {
-    $this->frame->add($this->lblDoLogToFile = new Label('log to file:', new Point(10, 15), new Dimension(15, 150)));
+    $this->frame->add($this->lblDoLogToFile = new Label('log to file:', new Point(10, 15), new Dimension(100, 15)));
     $this->frame->add($this->chkDoLogToFile = new Checkbox(0, new Point(152, 10), new Dimension(25, 25)));
-    
-    $this->lblDoLogToFile->getDimension()->resizeTo(new Dimension(150, 150));
     
     $this->frame->add($this->txtLog = new EditArea('', new Point(10, 40), new Dimension(280, 240)));
     $this->txtLog->setReadOnly(TRUE);
     
     $this->chkDoLogToFile->addActionListener(new ActionAdapter(function($event) {
-      if(!$event->getSource()->isChecked() && file_exists('./battman.log')) {
-        unlink('./battman.log');
+      $dialog = new FileSaveDialog('Please enter the file name where to store the log.');
+      $dialog->open();
+      if(($selection = $dialog->getSelection()) != null) {
+        $this->logFile = $selection;
       }
     }));
   }
@@ -52,8 +73,8 @@ class LogView extends View  {
     $this->txtLog->setValue(null);
     $this->txtLog->setValue($currentLog.$entry);
     
-    if($this->chkDoLogToFile->isChecked()) {
-      file_put_contents('./battman.log', trim($entry).PHP_EOL, FILE_APPEND);
+    if($this->chkDoLogToFile->isChecked() && $this->logFile != null) {
+      file_put_contents($this->logFile, trim($entry).PHP_EOL, FILE_APPEND);
     }
 
     unset($currentLog);
@@ -62,6 +83,7 @@ class LogView extends View  {
   }
 
   public function resizeBy(Dimension $delta) {
+    // no grid layout, so we manually resize the frame and the text area where to the log is shown
     $this->frame->resizeBy(new Dimension(0, $delta->height));
     $this->txtLog->resizeBy(new Dimension(0, $delta->height));
 
